bca2c010e801efa80ef4264738573ef32a59983c
Dewansh Sharma <dewansharma@Dewanshs-MacBook-Pro.local>
2023-04-27 21:11:03 -0400

    added both the fxns

--- Code/packet_parser.py
+++ Code/packet_parser.py
@@ -1,4 +1,6 @@
-def parse() :
-	print('called parse function in packet_parser.py')
+from filter_packets import get_icmp_data
 
-	
+icmp_data = get_icmp_data()
+
+for packet in icmp_data:
+    print(packet[0] + " " + packet[1] + " " + packet[2] + " " + packet[3] + " " + packet[4] + " " + packet[5])
