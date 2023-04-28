3f47964d38418c0aa41ad82f32c9ecb611fefee6
Dewansh Sharma <dewansharma@Dewanshs-MacBook-Pro.local>
2023-04-27 21:19:40 -0400

    updated

--- Code/filter_packets.py
+++ Code/filter_packets.py
@@ -1,9 +1,13 @@
-def get_icmp_data():
+def get_icmp_packets():
     icmp_data = []
+
     with open('PacketCaptureAnalysisTool/Captures/Node1.txt', 'r') as f:
+
         lines = f.readlines()
         for i in range(len(lines)):
+
             if "ICMP" in lines[i]:
+
                 data = lines[i].split()
                 time = data[1]
                 source_ip = data[2]
@@ -12,5 +16,6 @@ def get_icmp_data():
                 length = data[5]
                 info = " ".join(data[6:])
                 icmp_data.append([time, source_ip, dest_ip, protocol, length, info])
+
     return icmp_data
 
--- Code/packet_parser.py
+++ Code/packet_parser.py
@@ -1,6 +1,6 @@
-from filter_packets import get_icmp_data
+from filter_packets import get_icmp_packets
 
-icmp_data = get_icmp_data()
+icmp_data = get_icmp_packets()
 
 for packet in icmp_data:
     print(packet[0] + " " + packet[1] + " " + packet[2] + " " + packet[3] + " " + packet[4] + " " + packet[5])
